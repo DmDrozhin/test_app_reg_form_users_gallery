880cf12dfd2e5ee17c58e54ba28cca42d908772d
Dmitriy Drozhin <ddrozhin@gmail.com>
2023-12-15 18:46:40 +0200

    final 	modified:   .env 	modified:   .gitignore 	deleted:    application-letter.pdf 	deleted:    dist/css/404.39463702.css 	deleted:    dist/css/app.030081a7.css 	deleted:    dist/img/head-icon.0902afb7.svg 	modified:   dist/index.html 	deleted:    dist/js/404.e7f20ed6.js 	deleted:    dist/js/404.e7f20ed6.js.map 	deleted:    dist/js/app.bc6829a2.js 	deleted:    dist/js/app.bc6829a2.js.map 	modified:   vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/',
-  // publicPath: process.env.NODE_ENV === 'production' ? '/test-work-abz-agency' : '/',
+  // publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/test_app_reg_form_users_gallery' : '/',
 
 })
